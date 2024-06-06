7aa92496fd0bff633c6a1b714b4b45fbbdc6f353
Yohanes Teshome Kebede <johnteshe13@gmail.com>
2024-06-06 12:07:12 +0300

    Added split_text_into_chunks function

--- scripts/data_generation.py
+++ scripts/data_generation.py
@@ -87,6 +87,20 @@ def file_reader(path):
         raise
 
 
+def split_text_into_chunks(text, chunk_size=200):
+    try:
+        words = text.split()
+        chunks = [' '.join(words[i:i+chunk_size]) for i in range(0, len(words), chunk_size)]
+        
+        logger.info(f'Successfully chunked {len(chunks)}')
+
+        return chunks
+
+    except Exception as e:
+        logger.error(f"Error chunking the text file {e}")
+
+
+
 
 
 
